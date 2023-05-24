4eba47a0a3810f42c2f5e2d5854c8702144bcc45
Gümrah Sindar <gsindar@gmail.com>
2023-05-24 17:12:56 +0300

    finish header section

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className='mx-5 px-5 text-red-100'>Hello World</h1>
-    </>
+    <div className='container mx-auto grid min-h-screen  justify-center'>
+      <div className='mx-8'>
+        <header className=' my-20 text-center tracking-wide'>
+          <h1 className='mb-2 text-2xl  font-extralight text-neutral-darkBlue'>
+            Reliable, efficient delivery
+          </h1>
+          <h2 className='mb-4  text-2xl font-semibold text-neutral-darkBlue'>
+            Powered by Technology
+          </h2>
+          <p className='text-sm leading-6 opacity-50'>
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful
+          </p>
+        </header>
+      </div>
+    </div>
   )
 }
 
