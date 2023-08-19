b5af84132f95306865c53f36b3e709d85f87a02e
e.yari@inotek.ir <e.yari@inotek.ir>
2023-08-19 17:55:38 +0330

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,91 @@
-import './App.css'
-
 function App() {
   return (
-    <div className='container mx-auto my-20 grid  max-w-7xl justify-center'>
-      <div className='mx-8'>
-        <header className='mx-auto mb-20 max-w-xl text-center tracking-wide md:mb-16'>
-          <h1 className='mb-2 text-2xl font-extralight  text-neutral-darkBlue md:text-4xl'>
+    <div className="container mx-auto my-20 grid  max-w-7xl justify-center">
+      <div className="mx-8">
+        <header className="mx-auto mb-20 max-w-xl text-center tracking-wide md:mb-16 pt-2">
+          <h1 className="mb-2 text-2xl font-extralight  text-neutral-darkBlue md:text-4xl">
             Reliable, efficient delivery
           </h1>
-          <h2 className='mb-4  text-2xl font-semibold text-neutral-darkBlue md:text-4xl'>
+          <h2 className="mb-4  text-2xl font-semibold text-neutral-darkBlue md:text-4xl">
             Powered by Technology
           </h2>
-          <p className='leading-6 opacity-50'>
+          <p className="leading-6 opacity-50">
             Our Artificial Intelligence powered tools use millions of project
             data points to ensure that your project is successful
           </p>
         </header>
         <main>
           <section
-            id='card_section'
-            className='grid items-center gap-6 md:grid-flow-col md:grid-cols-3 md:gap-8 '
+            id="card_section"
+            className="grid items-center gap-6 md:grid-flow-col md:grid-cols-3 md:gap-8 "
           >
-            <div className='md:row-span-2'>
+            <div className="md:row-span-2">
               {/* card-1 */}
-              <div className='flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-cyan bg-white p-7 shadow-2xl'>
+              <div className="flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-cyan bg-white p-7 shadow-2xl">
                 {/* card heading */}
-                <div className='mb-8'>
-                  <h3 className='mb-1.5 text-xl font-semibold text-neutral-darkBlue'>
+                <div className="mb-8">
+                  <h3 className="mb-1.5 text-xl font-semibold text-neutral-darkBlue">
                     Supervisor
                   </h3>
-                  <p className='text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50'>
+                  <p className="text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50">
                     Monitors activity to identify project roadblocks
                   </p>
                 </div>
                 {/* card image */}
-                <div className='self-end'>
-                  <img src='icon-supervisor.svg' alt='' />
+                <div className="self-end">
+                  <img className='w-14' src="/icon-supervisor.svg" alt="" />
                 </div>
               </div>
             </div>
             {/* card-2 */}
-            <div className='flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-red bg-white  p-7 shadow-xl'>
+            <div className="flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-red bg-white  p-7 shadow-xl">
               {/* card heading */}
-              <div className='mb-8'>
-                <h3 className='mb-1.5 text-xl font-semibold text-neutral-darkBlue'>
+              <div className="mb-8">
+                <h3 className="mb-1.5 text-xl font-semibold text-neutral-darkBlue">
                   Team Builder
                 </h3>
-                <p className='text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50'>
+                <p className="text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50">
                   Scans our talent network to create the optimal team for your
                   project
                 </p>
               </div>
               {/* card image */}
-              <div className='self-end'>
-                <img src='icon-team-builder.svg' alt='' />
+              <div className="self-end">
+                <img className='w-14' src="/icon-team-builder.svg" alt="" />
               </div>
             </div>
             {/* card-3 */}
-            <div className='flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-orange bg-white  p-7 shadow-xl'>
+            <div className="flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-orange bg-white  p-7 shadow-xl">
               {/* card heading */}
-              <div className='mb-8'>
-                <h3 className='mb-1.5 text-xl font-semibold text-neutral-darkBlue'>
+              <div className="mb-8">
+                <h3 className="mb-1.5 text-xl font-semibold text-neutral-darkBlue">
                   Karma
                 </h3>
-                <p className='text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50'>
+                <p className="text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50">
                   Regularly evaluates our talent to ensure quality
                 </p>
               </div>
               {/* card image */}
-              <div className='self-end'>
-                <img src='icon-karma.svg' alt='' />
+              <div className="self-end">
+                <img className='w-14' src="/icon-karma.svg" alt="" />
               </div>
             </div>
-            <div className='md:row-span-2'>
+            <div className="md:row-span-2">
               {/* card-4 */}
-              <div className='flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-blue bg-white p-7 shadow-xl'>
+              <div className="flex flex-col rounded-lg rounded-t-md border-t-4 border-primary-blue bg-white p-7 shadow-xl">
                 {/* card heading */}
-                <div className='mb-8'>
-                  <h3 className='mb-1.5 text-xl font-semibold text-neutral-darkBlue'>
+                <div className="mb-8">
+                  <h3 className="mb-1.5 text-xl font-semibold text-neutral-darkBlue">
                     Calculator
                   </h3>
-                  <p className='text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50'>
+                  <p className="text-sm leading-6 tracking-wide text-neutral-darkBlue opacity-50">
                     Uses data from past projects to provide better delivery
                     estimates
                   </p>
                 </div>
                 {/* card image */}
-                <div className='self-end'>
-                  <img src='icon-calculator.svg' alt='' />
+                <div className="self-end">
+                  <img className='w-14' src="/icon-calculator.svg" alt="" />
                 </div>
               </div>
             </div>
@@ -95,7 +93,7 @@ function App() {
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
